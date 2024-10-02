bcfb2eea8aee118d0ef8f90267e44d01c0addd32
alejandro.nieto <alejandro.nieto.gallego0@gmail.com>
2024-10-02 23:48:29 +0200

    Added localhost as default network for hardhat.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,10 +2,15 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "localhost",
   solidity: "0.8.27",
   networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
     hardhat: {},
   },
 };
+
 
 export default config;
