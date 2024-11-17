6e86683739c6538d2eabb209c15f4312dfbb3996
alejandro.nieto <alejandro.nieto.gallego0@gmail.com>
2024-11-17 17:52:52 +0100

    Fixed hardhat account to the one of the issuer to block any other addresses trying to revoke. Fixed proxy pattern: the data was being store at the logic contract, not at the proxy as it should.

--- contracts/TitulacionDigitalProxy.sol
+++ contracts/TitulacionDigitalProxy.sol
@@ -8,15 +8,23 @@ contract TitulacionDigitalProxy {
     /// @notice Address of the current implementation contract.
     /// @dev This address is used by the proxy to delegate calls to the current implementation.
     address public implementation;
+    /// @notice Address of the current admin of the revokation registry.
+    address public admin;
+    /// @notice Mapping to store the revocation status of each titulacion.
+    /// @dev Maps a hashed titulacion ID (`bytes32`) to a `bool` indicating whether the titulacion is revoked (`true`) or not (`false`).
+    mapping(bytes32 => bool) public titulacionesRevocadas;
+
 
     /// @notice Event emitted when the implementation contract is upgraded.
     /// @param implementation The address of the new implementation contract.
     event Upgraded(address indexed implementation);
 
     /// @notice Initializes the proxy with the initial implementation contract.
+    /// @param _admin The address of the admin of the revokation registry.
     /// @param _implementation The address of the initial implementation contract.
-    constructor(address _implementation) {
+    constructor(address _admin, address _implementation) {
         require(_implementation != address(0), "Invalid implementation address");
+        admin = _admin;
         implementation = _implementation;
     }
 
@@ -29,7 +37,7 @@ contract TitulacionDigitalProxy {
     /// @notice Upgrades the implementation contract to a new address.
     /// @dev This function updates the implementation address, allowing the proxy to point to new logic. Access control should be added in production to restrict who can call this function.
     /// @param newImplementation The address of the new implementation contract.
-    function upgrade(address newImplementation) external {
+    function upgrade(address newImplementation) external onlyAdmin {
         require(newImplementation != address(0), "Invalid implementation address");
         implementation = newImplementation;
         emit Upgraded(newImplementation);
@@ -63,5 +71,11 @@ contract TitulacionDigitalProxy {
                 return(0, returndatasize())
             }
         }
+    }
+
+    /// @notice Modifier to restrict access to only the admin address.
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Access restricted to the issuer that owns the registry");
+        _;
     }
 }
